597cad3caba87650f082ed4965de1437a9b3ba74
Bona <bjorn.fyrvall@gmail.com>
2020-01-19 01:52:24 +0100

    Added examples. Added AttributeUsage attribute to PreviewPickerAttribute.cs

--- Assets/PreviewObjectPicker/PreviewPickerAttribute.cs
+++ Assets/PreviewObjectPicker/PreviewPickerAttribute.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Fyrvall.PreviewObjectPicker
 {
+    [AttributeUsage(AttributeTargets.Field)]
     public class PreviewPickerPropertyAttribute : PropertyAttribute { }
 }
